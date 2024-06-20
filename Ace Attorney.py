5332cd41bec389934e1f4bc346f3ec33922e122c
AlexanderGorgonzola <alex.garakanidze@protonmail.com>
2024-06-20 12:14:47 -0700

    the start of a legend that died shortly after I guess

--- Ace Attorney.py
+++ Ace Attorney.py
@@ -29,6 +29,7 @@ class AceAttorney:
 
     def _update_screen(self):
         if self.stats.game_active:
+            self.update_turns()
             if self.character.turn == "witness 2": #yippie, A MESS!
                 self.settings.current_bg = "witness"
             else:
@@ -68,6 +69,17 @@ class AceAttorney:
                 self._check_play_button(mouse_pos)
                 self._check_normal_buttons(mouse_pos)
                 pygame.display.flip()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.settings.turn_des == "Start":
+                        self.settings.turn_des = "Start_2"
+    def update_turns(self):
+        if self.settings.turn_des == "Start":
+            self.character.turn == "judge"
+        elif self.settings.turn_des == "Start 2":
+            self.character.turn == "defense"
+        elif self.settings.turn_des == "Start 3":
+            self.character.turn == "prosecutor"
     def _check_play_button(self, mouse_pos):
         intro_button_clicked = self.buttons.button_rect.collidepoint(mouse_pos)
         if intro_button_clicked and not self.stats.game_active:
--- character_text.py
+++ character_text.py
@@ -8,18 +8,24 @@ class CharacterText:
         self.screen = at_game.screen
         self.screen_rect = self.screen.get_rect()
         self.character = Character(self)
-        self.judge_lines = ["Court is now in Session", "What do you mean by that?", "Interesting...",
+        self.judge_lines = ["Court is now in Session.", "Is everyone ready?", "What do you mean by that?", "Interesting...",
                             "Who would have known?", "I pass the verdict: Guilty", "I pass the verdict: Not Guilty"]
-        self.font = pygame.font.SysFont("monospace", 30)
-        self.text = "PLACEHOLDER :)"
-        self.text_rect = "OTHER PLACEHOLDER :)"
+        self.font = pygame.font.SysFont("lucidaconsole", 25)
+        self.text_1 = "PLACEHOLDER :)"
+        self.text_1_rect = "OTHER PLACEHOLDER :)"
+        self.text_2 = "PLACEHOLDER :)"
+        self.text_2_rect = "OTHER PLACEHOLDER :)"
     def prep_speech(self, turn, moves):
         if turn == "judge":
             if moves == "Start":
-                wrapped_text = textwrap.fill(self.judge_lines[0], width=500)
-
-        self.text = self.font.render(wrapped_text, True, (255, 255, 255))
-        self.text_rect = self.text.get_rect()
-        self.text_rect.midbottom = self.screen_rect.midbottom
+                self.text_1 = self.font.render(self.judge_lines[0], True, (0, 0, 0))
+                self.text_2 = self.font.render(self.judge_lines[1], True, (0, 0, 0))
+                self.text_1_rect = self.text_1.get_rect()
+                self.text_2_rect = self.text_2.get_rect()
+                self.text_1_rect.right = self.screen_rect.right - 240
+                self.text_1_rect.bottom = self.screen_rect.bottom - 120
+                self.text_2_rect.right = self.screen_rect.right - 330
+                self.text_2_rect.bottom = self.screen_rect.bottom - 90
     def draw_speech(self):
-        self.screen.blit(self.text, self.text_rect)
+        self.screen.blit(self.text_1, self.text_1_rect)
+        self.screen.blit(self.text_2, self.text_2_rect)
